5b6a879f669ac4704618e3912b1d84590dce476d
y-nagai0725 <y.nagai0725@gmail.com>
2025-07-14 23:25:55 +0900

    天気予報情報取得テスト

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-fetch('https://jsonplaceholder.typicode.com/users/2')
+fetch('https://www.jma.go.jp/bosai/forecast/data/overview_forecast/016000.json')
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    document.querySelector(".test").textContent = data.name;
+    document.querySelector(".test").textContent = data.text;
   })
   .catch(error => {
     console.error('データの取得に失敗しちゃった…', error);
