173c98f4a67ac14118212858d2f2e023247b3a43
y-nagai0725 <y.nagai0725@gmail.com>
2025-07-22 23:58:51 +0900

    データベース接続準備

--- js/script.js
+++ js/script.js
@@ -7,3 +7,32 @@ fetch('https://www.jma.go.jp/bosai/forecast/data/overview_forecast/016000.json')
   .catch(error => {
     console.error('データの取得に失敗しちゃった…', error);
   });
+
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',              //データーベースを操作するユーザー名を記述する
+    password: '',          //操作するユーザーのパスワードを記述する
+    database: 'hotel',          //操作するデーターベース名を記述
+    // 同時接続数の制限（任意）をかけるために記述
+    //connectionLimit: 5
+});
+
+pool.getConnection()
+  .then(conn => {
+    conn.query('select * from wp_users')
+      .then(results => {   //データーベースからデーターを取得できた場合の処理
+        console.log(results)
+      })
+      .catch(error => {   //データーベースの接続を失敗したときの処理
+        console.log(error);
+      })
+      .finally(() => { //最後に必ず行う処理
+        //接続を終了する
+        if (conn) conn.end();
+      });
+
+  }).catch((error) => {
+    console.log(error);
+  });
