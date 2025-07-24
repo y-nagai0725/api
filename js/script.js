d1aad844eb7453fa7e267ffbc0bf4886b3e6bfe2
y-nagai0725 <y.nagai0725@gmail.com>
2025-07-24 20:54:38 +0900

    データベース接続しないので色々削除。localstorageへの保存、情報取得のテスト等

--- js/script.js
+++ js/script.js
@@ -1,40 +1,36 @@
-import mariadb from "mariadb";
+const selectedCity = localStorage.getItem("selectedCity");
 
-fetch('https://www.jma.go.jp/bosai/forecast/data/overview_forecast/016000.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    document.querySelector(".test").textContent = data.text;
-  })
-  .catch(error => {
-    console.error('データの取得に失敗しちゃった…', error);
+if (selectedCity) {
+  console.log("既に選択しているよ");
+  document.querySelector(".test2").textContent = selectedCity;
+  getWeatherData(selectedCity);
+  Array.from(document.querySelector(".select").options).forEach(option => {
+    const value = option.value;
+    if(selectedCity === value){
+      option.selected = true;
+    }
   });
+} else {
+  console.log("まだ選択していないよ");
+  document.querySelector(".test2").textContent = "選択してください";
+}
 
-//const mariadb = require('mariadb');
-
-const pool = mariadb.createPool({
-    host: 'localhost',
-    user: 'root',              //データーベースを操作するユーザー名を記述する
-    password: '',          //操作するユーザーのパスワードを記述する
-    database: 'hotel',          //操作するデーターベース名を記述
-    // 同時接続数の制限（任意）をかけるために記述
-    //connectionLimit: 5
+document.querySelector(".select").addEventListener("change", () => {
+  console.log("change!!");
+  const city = document.querySelector(".select").value;
+  document.querySelector(".test2").textContent = city;
+  localStorage.setItem("selectedCity", city);
+  getWeatherData(city);
 });
 
-pool.getConnection()
-  .then(conn => {
-    conn.query('select * from wp_users')
-      .then(results => {   //データーベースからデーターを取得できた場合の処理
-        console.log(results)
-      })
-      .catch(error => {   //データーベースの接続を失敗したときの処理
-        console.log(error);
-      })
-      .finally(() => { //最後に必ず行う処理
-        //接続を終了する
-        if (conn) conn.end();
-      });
-
-  }).catch((error) => {
-    console.log(error);
-  });
+function getWeatherData(cityNumber) {
+  fetch(`https://www.jma.go.jp/bosai/forecast/data/overview_forecast/${cityNumber}.json`)
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      document.querySelector(".test").textContent = data.text;
+    })
+    .catch(error => {
+      console.error('データの取得に失敗しちゃった…', error);
+    });
+}
