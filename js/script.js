911b88864d9431466dca0c3343d2cfcfd646b626
y-nagai0725 <y.nagai0725@gmail.com>
2025-07-30 19:15:07 +0900

    天気概要の情報表示テストなど

--- js/script.js
+++ js/script.js
@@ -2,35 +2,43 @@ const selectedCity = localStorage.getItem("selectedCity");
 
 if (selectedCity) {
   console.log("既に選択しているよ");
-  document.querySelector(".test2").textContent = selectedCity;
   getOverviewWeatherData(selectedCity);
   getDetailWeatherData(selectedCity);
   Array.from(document.querySelector(".select").options).forEach(option => {
     const value = option.value;
-    if(selectedCity === value){
+    if (selectedCity === value) {
       option.selected = true;
     }
   });
 } else {
   console.log("まだ選択していないよ");
-  document.querySelector(".test2").textContent = "選択してください";
 }
 
 document.querySelector(".select").addEventListener("change", () => {
   console.log("change!!");
   const city = document.querySelector(".select").value;
-  document.querySelector(".test2").textContent = city;
-  localStorage.setItem("selectedCity", city);
-  getOverviewWeatherData(city);
-  getDetailWeatherData(city);
+  if (city === "") {
+    localStorage.removeItem("selectedCity");
+  } else {
+    localStorage.setItem("selectedCity", city);
+    getOverviewWeatherData(city);
+    getDetailWeatherData(city);
+  }
 });
 
 function getOverviewWeatherData(cityNumber) {
   fetch(`https://www.jma.go.jp/bosai/forecast/data/overview_forecast/${cityNumber}.json`)
     .then(response => response.json())
     .then(data => {
-      //console.log(data);
-      document.querySelector(".test").textContent = data.text;
+      console.log(data);
+      const publisher = data.publishingOffice;
+      const reportDateTime = data.reportDatetime;
+      //const area = data.targetArea;
+      const overviewText = data.text;
+
+      document.querySelector(".publisher").textContent = publisher;
+      document.querySelector(".reportDateTime").textContent = reportDateTime;
+      document.querySelector(".overviewText").textContent = overviewText;
     })
     .catch(error => {
       console.error('データの取得に失敗しちゃった…', error);
